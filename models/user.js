5f394d89a706036b81bc63450694861d83c043d2
1styrGroupB <1styrGroupB@pn.local>
2020-12-09 21:29:23 +0800

    controller

--- models/user.js
+++ models/user.js
@@ -2,5 +2,12 @@ const { Schema } = require("mongoose")
 
 const mongoose = require|('mongoose')
 
+// mongoose.connect('mongodb://localhost:27017/userdb')
 
-const userSchema = new 
+const userSchema = new mongoose.Schema({
+    username : {type: String, required : true},
+    email : {type: String, required: true},
+    password : {type: String, required: true},
+})
+
+module.export = userSchema
