cba8ee13eff55d0b696c604e642ad3218f07ab6f
1styrGroupB <1styrGroupB@pn.local>
2020-12-09 20:39:08 +0800

    so far

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = 4000
+
+app.use(express.static('public'))
+app.set('veiw engine', 'ejs')
+
+app.get('/', (req, res) =>{
+    res. render("index")
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}`)
+})
